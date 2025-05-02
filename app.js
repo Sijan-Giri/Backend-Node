1afd09327aacd746dd0a0c888035feda645d603a
Sijan-Giri <girisijan346@gmail.com>
2025-05-02 19:07:49 +0545

    .env file and IP address to database

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const connectDatabase = require("./database/db_connect");
 const app = express();
+require("dotenv").config();
 
 connectDatabase();
 
 app.get("/",(req,res) => {
-    res.status(200).json({ message : "We are working on it so please wait..."})
+    res.send( "We are working on it so please wait...")
 })
 
-app.listen("3000",() => {
+app.listen(process.env.PORT,() => {
     console.log("Server started at port 3000...")
 })
--- database/db_connect.js
+++ database/db_connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 const connectDatabase = async() => {
     try {
-        await mongoose.connect("mongodb+srv://girisijan346:sijan@cluster0.ynxrpx8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect(process.env.DATABASE_CODE)
     console.log("Database connected successfully");
     } catch (error) {
         console.log("Something went wrong!",error)
